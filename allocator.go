50cb91dce6e494e75d42ae25fd01bea1399574fb
Bruce Ma <brucema19901024@gmail.com>
2019-09-06 15:18:28 +0800

    [Allocator] implement generic allocator logics
    Signed-off-by: Bruce Ma <brucema19901024@gmail.com>

--- allocator.go
+++ allocator.go
@@ -16,9 +16,62 @@
 
 package allocator
 
+import "errors"
+
 type Interface interface {
 	Assign(RangeID) error
 	Allocate() (RangeID, interface{}, error)
 	Release(RangeID) error
 	Has(RangeID) bool
+}
+
+var (
+	ErrFull       = errors.New("range is full")
+	ErrAllocated  = errors.New("already allocated")
+	ErrOutOfRange = errors.New("out of range")
+)
+
+type allocator struct {
+	ranger Range
+	store  map[RangeID]struct{}
+}
+
+func NewAllocator(ranger Range) Interface {
+	return &allocator{
+		ranger: ranger,
+		store:  make(map[RangeID]struct{}),
+	}
+}
+
+func (a *allocator) Assign(id RangeID) error {
+	if ! a.ranger.Contains(id) {
+		return ErrOutOfRange
+	}
+	if a.Has(id) {
+		return ErrAllocated
+	}
+	a.store[id] = struct{}{}
+	return nil
+}
+
+func (a *allocator) Allocate() (RangeID, interface{}, error) {
+	for ri := a.ranger.First(); ri.InRange(); ri.Next() {
+		id, value := ri.Get()
+		if a.Assign(id) == nil {
+			return id, value, nil
+		}
+	}
+	return "", nil, ErrFull
+}
+
+func (a *allocator) Release(id RangeID) error {
+	delete(a.store, id)
+	return nil
+}
+
+func (a *allocator) Has(id RangeID) bool {
+	if _, exist := a.store[id]; exist {
+		return true
+	}
+	return false
 }
