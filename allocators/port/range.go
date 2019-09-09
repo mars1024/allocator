5958e4707542645f5aab8e4cd3f20bb9b0d94c5f
Bruce Ma <brucema19901024@gmail.com>
2019-09-09 20:49:15 +0800

    [PortAllocator] implement port ranger and iterator
    Signed-off-by: Bruce Ma <brucema19901024@gmail.com>

---
+++ allocators/port/range.go
@@ -0,0 +1,76 @@
+/*
+ Copyright 2019 Bruce Ma
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+*/
+
+package port
+
+import (
+	"errors"
+	"strconv"
+
+	"github.com/mars1024/allocator"
+)
+
+var (
+	ErrBadRangeID = errors.New("bad rangeID")
+)
+
+type ranger struct {
+	base   int
+	offset int
+}
+
+func parseRangeID(rangeID allocator.RangeID) (int, error) {
+	port, err := strconv.Atoi(string(rangeID))
+	if err != nil {
+		return 0, ErrBadRangeID
+	}
+
+	return port, nil
+}
+
+func generateRangeID(port int) allocator.RangeID {
+	return allocator.RangeID(strconv.Itoa(port))
+}
+
+func (r *ranger) Contains(rangeID allocator.RangeID) bool {
+	port, err := parseRangeID(rangeID)
+
+	return (err == nil) && (port >= r.base) && (port <= (r.base + r.offset))
+}
+
+func (r *ranger) First() allocator.RangeIterator {
+	return &iterator{
+		ranger: r,
+		cur:    r.base,
+	}
+}
+
+type iterator struct {
+	ranger *ranger
+	cur    int
+}
+
+func (i *iterator) Get() (allocator.RangeID, interface{}) {
+	return generateRangeID(i.cur), i.cur
+}
+
+func (i *iterator) Next() {
+	i.cur++
+}
+
+func (i *iterator) InRange() bool {
+	return (i.cur >= i.ranger.base) && (i.cur <= (i.ranger.base + i.ranger.offset))
+}
